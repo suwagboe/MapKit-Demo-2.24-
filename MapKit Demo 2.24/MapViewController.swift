97111be4a8899b81d35a04395d9687487e4ff4e9
uwagboe <sm.uwagboe@gmail.com>
2020-02-24 10:41:28 -0500

    Initial Commit

---
+++ MapKit Demo 2.24/MapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MapKit Demo 2.24
+//
+//  Created by Pursuit on 2/24/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+    private let locationSession = CoreLocationSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           // lets attempt to show the user's location
+        mapView.showsUserLocation = true
+        
+        textField.delegate = self
+
+    }
+    
+
+}
+
+
+extension MapViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // searchText is also the place name
+        guard let searchText = textField.text, !searchText.isEmpty else {
+            
+            return true
+        }
+        
+        return true
+        
+    }
+}
---
+++ MapKit Demo 2.24Tests/MapKit_Demo_2_24Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MapKit_Demo_2_24Tests.swift
+//  MapKit Demo 2.24Tests
+//
+//  Created by Pursuit on 2/24/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import XCTest
+@testable import MapKit_Demo_2_24
+
+class MapKit_Demo_2_24Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
